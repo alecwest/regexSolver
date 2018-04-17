e8ce0b801ee780cd21c8e1f9a1e4abb8e0424842
Alec West <alec.west@adtran.com>
2018-04-17 16:22:15 -0500

    Add TestAddExpression

--- regexrow_test.go
+++ regexrow_test.go
@@ -1,6 +1,9 @@
 package regexsolver
 
-import "testing"
+import (
+	"regexp"
+	"testing"
+)
 
 func TestAddCell(t *testing.T) {
 	var row RegexRow
@@ -13,8 +16,27 @@ func TestAddCell(t *testing.T) {
 	}
 	for _, table := range tables {
 		row.AddCell(table.cell)
-		if len(row.Cells) != table.numExpected {
-			t.Errorf("RegexCell was not sucessfully added. Counted %d, expected %d", len(row.Cells), table.numExpected)
+		res := len(row.Cells)
+		if res != table.numExpected {
+			t.Errorf("RegexCell was not sucessfully added. Counted %d, expected %d", res, table.numExpected)
+		}
+	}
+}
+
+func TestAddExpression(t *testing.T) {
+	var row RegexRow
+	tables := []struct {
+		expression  *regexp.Regexp
+		numExpected int
+	}{
+		{regexp.MustCompile("a"), 1},
+		{regexp.MustCompile("[a-z]"), 2},
+	}
+	for _, table := range tables {
+		row.AddExpression(table.expression)
+		res := len(row.Expressions)
+		if res != table.numExpected {
+			t.Errorf("Regex expression was not successufully added. Counted %d, expected %d", res, table.numExpected)
 		}
 	}
 }
