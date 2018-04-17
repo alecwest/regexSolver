9d9a89a63321f7779f027ce78a9d3d862ace6191
Alec West <alec.west@adtran.com>
2018-04-17 16:15:36 -0500

    Add TestAddCell

--- regexrow_test.go
+++ regexrow_test.go
@@ -1,1 +1,20 @@
 package regexsolver
+
+import "testing"
+
+func TestAddCell(t *testing.T) {
+	var row RegexRow
+	tables := []struct {
+		cell        *RegexCell
+		numExpected int
+	}{
+		{&RegexCell{content: "a"}, 1},
+		{&RegexCell{content: ""}, 2},
+	}
+	for _, table := range tables {
+		row.AddCell(table.cell)
+		if len(row.Cells) != table.numExpected {
+			t.Errorf("RegexCell was not sucessfully added. Counted %d, expected %d", len(row.Cells), table.numExpected)
+		}
+	}
+}
