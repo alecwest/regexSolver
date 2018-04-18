add533db51f39206249151fe461d0ca5033c1603
Alec West <alec.west@adtran.com>
2018-04-18 09:03:13 -0500

    Add tests for validation and join

--- regexrow_test.go
+++ regexrow_test.go
@@ -40,3 +40,41 @@ func TestAddExpression(t *testing.T) {
 		}
 	}
 }
+
+func TestIsValidRow(t *testing.T) {
+	// TODO figure out how to make a table for this
+	var row1 RegexRow
+	var row2 RegexRow
+	row1.AddCell(&RegexCell{content: "a"})
+	row1.AddCell(&RegexCell{content: "b"})
+	row1.AddCell(&RegexCell{content: "c"})
+	row1.AddExpression(regexp.MustCompile("[a-z]+"))
+	row1.AddExpression(regexp.MustCompile("[^0-9]+"))
+	result1 := row1.IsValidRow()
+	expectedResult1 := true
+	if result1 != expectedResult1 {
+		t.Errorf("Wrong result got in validity test. Got %v, expected %v", result1, expectedResult1)
+	}
+
+	row2.AddCell(&RegexCell{content: "a"})
+	row2.AddCell(&RegexCell{content: "1"})
+	row2.AddCell(&RegexCell{content: "H"})
+	row2.AddExpression(regexp.MustCompile("[b-h]+"))
+	result2 := row2.IsValidRow()
+	expectedResult2 := false
+	if result2 != expectedResult2 {
+		t.Errorf("Wrong result got in validity test. Got %v, expected %v", result2, expectedResult2)
+	}
+}
+
+func TestJoin(t *testing.T) {
+	var row1 RegexRow
+	row1.AddCell(&RegexCell{content: "a"})
+	row1.AddCell(&RegexCell{content: "b"})
+	row1.AddCell(&RegexCell{content: "c"})
+	result1 := row1.join()
+	expectedResult1 := "abc"
+	if result1 != expectedResult1 {
+		t.Errorf("Wrong result got in join test. Got %v, expected %v", result1, expectedResult1)
+	}
+}
