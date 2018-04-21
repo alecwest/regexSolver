be1df6c0a34ae35c803140ccfcbf4ea07bfbe738
Alec West <alecwest38@gmail.com>
2018-04-21 09:01:24 -0500

    Write testEq test

--- helpers_test.go
+++ helpers_test.go
@@ -1,7 +1,49 @@
 package regexsolver
 
-import "testing"
+import (
+	"regexp"
+	"testing"
+)
 
-func TestTestEqOnNulls(t *testing.T) {
-
+func TestTestEq(t *testing.T) {
+	r1 := regexp.MustCompile("123")
+	r2 := regexp.MustCompile("456")
+	tables := []struct {
+		regex1   []*regexp.Regexp
+		regex2   []*regexp.Regexp
+		expected bool
+	}{
+		{nil, nil, true},
+		{
+			[]*regexp.Regexp{
+				r1,
+			},
+			nil,
+			false,
+		},
+		{
+			[]*regexp.Regexp{
+				r1,
+			},
+			[]*regexp.Regexp{
+				r1,
+			},
+			true,
+		},
+		{
+			[]*regexp.Regexp{
+				r1,
+			},
+			[]*regexp.Regexp{
+				r1,
+				r2,
+			},
+			false,
+		},
+	}
+	for _, table := range tables {
+		if testEq(table.regex1, table.regex2) != table.expected {
+			t.Errorf("Got unexpected result when comparing %s and %s. Got %v", table.regex1, table.regex2, !table.expected)
+		}
+	}
 }
