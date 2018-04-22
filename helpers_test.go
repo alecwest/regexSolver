12d8711574283b3ab0da9c41c869a1177e004885
Alec West <alecwest38@gmail.com>
2018-04-22 10:35:36 -0500

    Add TestIsEqCells

--- helpers_test.go
+++ helpers_test.go
@@ -47,7 +47,7 @@ func TestIsSolved(t *testing.T) {
 	}
 }
 
-func TestisEqRegex(t *testing.T) {
+func TestIsEqRegex(t *testing.T) {
 	r1 := regexp.MustCompile("123")
 	r2 := regexp.MustCompile("456")
 	tables := []struct {
@@ -63,6 +63,33 @@ func TestisEqRegex(t *testing.T) {
 	for _, table := range tables {
 		if isEqRegex(table.regex1, table.regex2) != table.expected {
 			t.Errorf("Got unexpected result when comparing %s and %s. Got %v", table.regex1, table.regex2, !table.expected)
+		}
+	}
+}
+
+func TestIsEqCells(t *testing.T) {
+	c1 := RegexCell{"a"}
+	c2 := RegexCell{"b"}
+	c3 := RegexCell{"c"}
+	tables := []struct {
+		row1     RegexRow
+		row2     RegexRow
+		expected bool
+	}{
+		{
+			RegexRow{[]*RegexCell{&c1, &c2, &c3}, []*regexp.Regexp{}},
+			RegexRow{[]*RegexCell{&c1, &c2, &c3}, []*regexp.Regexp{}},
+			true,
+		},
+		{
+			RegexRow{[]*RegexCell{&c1, &c2, &c3}, []*regexp.Regexp{}},
+			RegexRow{[]*RegexCell{&c1, &RegexCell{"d"}, &c3}, []*regexp.Regexp{}},
+			false,
+		},
+	}
+	for _, table := range tables {
+		if isEqCells(table.row1.Cells, table.row2.Cells) != table.expected {
+			t.Errorf("Unexpected result from isEqCells when comparing %s and %s. Got %v", table.row1, table.row2, !table.expected)
 		}
 	}
 }
