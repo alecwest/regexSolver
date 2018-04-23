15c3b8d93826b73691596b32d972527b5ef2819f
Alec West <alecwest38@gmail.com>
2018-04-22 19:51:50 -0500

    Make TestIsValidWithNewCell pass

--- helpers_test.go
+++ helpers_test.go
@@ -170,38 +170,45 @@ func TestIsValid(t *testing.T) {
 }
 
 func TestIsValidWithNewCell(t *testing.T) {
+	c1 := &RegexCell{"h"}
+	c2 := &RegexCell{""}
+	c3 := &RegexCell{"l"}
+	c4 := &RegexCell{"p"}
+	c5 := &RegexCell{"e"}
+	c6 := &RegexCell{""}
 	tables := []struct {
 		puzzle   *RegexPuzzle
-		cell     *RegexCell
+		newValue string
 		expected bool
 	}{
 		{
 			// Sample table from https://regexcrossword.com/challenges/beginner/puzzles/1
 			&RegexPuzzle{
-				[]*RegexCell{{"h"}, {""}, {"l"}, {"p"}},
+				[]*RegexCell{c1, c2, c3, c4},
 				[]*RegexRow{
-					{[]*RegexCell{{"h"}, {""}}, []*regexp.Regexp{regexp.MustCompile("he|ll|o+")}},
-					{[]*RegexCell{{"l"}, {"p"}}, []*regexp.Regexp{regexp.MustCompile("[please]+")}},
-					{[]*RegexCell{{"h"}, {"l"}}, []*regexp.Regexp{regexp.MustCompile("[^speak]+")}},
-					{[]*RegexCell{{""}, {"p"}}, []*regexp.Regexp{regexp.MustCompile("ep|ip|ef")}},
+					{[]*RegexCell{c1, c2}, []*regexp.Regexp{regexp.MustCompile("he|ll|o+")}},
+					{[]*RegexCell{c3, c4}, []*regexp.Regexp{regexp.MustCompile("[please]+")}},
+					{[]*RegexCell{c1, c3}, []*regexp.Regexp{regexp.MustCompile("[^speak]+")}},
+					{[]*RegexCell{c2, c4}, []*regexp.Regexp{regexp.MustCompile("ep|ip|ef")}},
 				},
-			}, &RegexCell{"e"}, true,
+			}, "e", true,
 		},
 		{
 			&RegexPuzzle{
-				[]*RegexCell{{"h"}, {"e"}, {"l"}, {""}},
+				[]*RegexCell{c1, c5, c3, c6},
 				[]*RegexRow{
-					{[]*RegexCell{{"h"}, {"e"}}, []*regexp.Regexp{regexp.MustCompile("he|ll|o+")}},
-					{[]*RegexCell{{"l"}, {""}}, []*regexp.Regexp{regexp.MustCompile("[please]+")}},
-					{[]*RegexCell{{"h"}, {"l"}}, []*regexp.Regexp{regexp.MustCompile("[^speak]+")}},
-					{[]*RegexCell{{"e"}, {""}}, []*regexp.Regexp{regexp.MustCompile("ep|ip|ef")}},
+					{[]*RegexCell{c1, c5}, []*regexp.Regexp{regexp.MustCompile("he|ll|o+")}},
+					{[]*RegexCell{c3, c6}, []*regexp.Regexp{regexp.MustCompile("[please]+")}},
+					{[]*RegexCell{c1, c3}, []*regexp.Regexp{regexp.MustCompile("[^speak]+")}},
+					{[]*RegexCell{c5, c6}, []*regexp.Regexp{regexp.MustCompile("ep|ip|ef")}},
 				},
-			}, &RegexCell{"d"}, false,
+			}, "d", false,
 		},
 	}
 	for _, table := range tables {
-		if isValidWithNewCell(table.cell, table.puzzle) != table.expected {
-			t.Errorf("Unexpected result from isValidWithNewCell function call on puzzle %s with cell %s, expected %v", table.puzzle, table.cell, table.expected)
+		newCell := &RegexCell{table.newValue}
+		if isValidWithNewCell(newCell, table.puzzle) != table.expected {
+			t.Errorf("Unexpected result from isValidWithNewCell function call on puzzle %s with cell %s, expected %v", table.puzzle, newCell, table.expected)
 		}
 	}
 }
